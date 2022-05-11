be111cf08ee2b93c0108ae604ac8d1ea86136af1
Arieleketely <arieleketely2012@hotmail.com>
2022-05-11 10:55:23 -0300

    first commit

---
+++ Aula 19-Componente Obter Produto/nome-projeto/src/app/componets/template/product/product-create/product-create.component.ts
@@ -0,0 +1,35 @@
+import { Product } from './../product.module';
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+product:Product={
+  name:'Produto de texte',
+  price:125.50
+}
+  constructor(private productService:ProductService,
+    private router:Router) { }
+
+  ngOnInit(): void {
+ 
+  }
+createProduct():void{
+  this.productService.create(this.product).subscribe(()=>{
+
+
+  this.productService.showMensager('--------------------'+'Atenção Produto criado!')
+  this.router.navigate(['/product'])
+
+})
+
+}
+deleteProduct():void{
+  this.router.navigate(['/product'])
+}
+}
